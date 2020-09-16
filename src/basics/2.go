083f18753b753f9a7c40a95308bdbb8efb75fd99
Vince Chan <vince.chanl26@gmail.com>
2020-09-16 13:28:40 +0800

    multiple import statements

---
+++ src/basics/2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+// or imports into a parenthesized
+//import (
+//	"fmt"
+//	"math/rand"
+//)
+
+func main() {
+	fmt.Printf("%v, %f", 10, rand.Float64())
+}
